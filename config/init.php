7973da27d53972aa9e329b2ef3ed9451427d0a35
sergeisavelyev <sergeisavelyev11@gmail.com>
2022-11-14 15:24:13 +0400

    added configuration, autoloader

---
+++ config/init.php
@@ -0,0 +1,16 @@
+<?php
+define("DEBUG", 1);
+define("ROOT", dirname(__DIR__));
+define("WWW", ROOT . "/public");
+define("APP", ROOT . "/app");
+define("CORE", ROOT . "/vendor/core");
+define("HELPERS", ROOT . "/vendor/core/helpers");
+define("CACHE", ROOT . "/tmp/cache");
+define("LOGS", ROOT . "/tmp/logs");
+define("CONFIG", ROOT . "/congig");
+define("LAYOUT", ROOT . "layout");
+define("PATH", ROOT . "http://sitename");
+define("ADMIN", ROOT . "http://sitename/admin");
+define("NO_IMG", ROOT . "/public/images/no_image.jpg");
+
+require_once ROOT . "/vendor/autoload.php";
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+if (PHP_MAJOR_VERSION < 8) {
+    die('Версия PHP должна быть выше 8');
+}
+
+require_once dirname(__DIR__) . "/config/init.php";
