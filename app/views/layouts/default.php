d631d696ab89592b794efb5330ce7f5f1e0e96f2
sergeisavelyev <sergeisavelyev11@gmail.com>
2022-11-18 15:58:53 +0400

    added View

---
+++ app/views/layouts/default.php
@@ -0,0 +1,21 @@
+<?php
+
+use core\View;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Default</title>
+</head>
+
+<body>
+    <?= $this->content; ?>
+</body>
+
+</html>
--- config/init.php
+++ config/init.php
@@ -8,7 +8,7 @@ define("HELPERS", ROOT . "/vendor/core/helpers");
 define("CACHE", ROOT . "/tmp/cache");
 define("LOGS", ROOT . "/tmp/logs");
 define("CONFIG", ROOT . "/config");
-define("LAYOUT", ROOT . "layout");
+define("LAYOUT", "default");
 define("PATH", ROOT . "http://sitename");
 define("ADMIN", ROOT . "http://sitename/admin");
 define("NO_IMG", ROOT . "/public/images/no_image.jpg");
